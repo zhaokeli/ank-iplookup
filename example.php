91f209fbd34d91b387d5ad232bdd56db11bdc6f8
kelicom <735579768@qq.com>
2019-03-20 16:26:51 +0800

    add

--- example.php
+++ example.php
@@ -1,8 +1,3 @@
 <?php
-include __DIR__ . '/libs/IpLocation.php';
-
-//使用方法
-//默认有个数据库文件,也可以自己下载一个传入路径就可以下载ip UTFWry.dat 数据库文件在当前目录
-$iplocation = new \ank\IpLocation();
-$res        = $iplocation->getlocation('1.80.2.152');
-var_dump($res);
+$loader = require __DIR__ . '/vendor/autoload.php';
+var_dump(Zhuzhichao\IpLocationZh\Ip::find('113.140.250.222'));
